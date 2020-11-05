9d3f4416583c861f13682d646195b856f35e3d6c
Kar5799 <45506887+Kar5799@users.noreply.github.com>
2020-11-05 16:07:17 +0530

    Add files via upload

---
+++ PyAss2/PyAss2Q1.py
@@ -0,0 +1,7 @@
+for i in range(0,2):
+    if i == 0:
+        for j in range(1,6):
+            print('*'*j)
+    else:
+        for j in range(1,5)[::-1]:
+            print('*'*j)
---
+++ PyAss2/PyAss2Q2.py
@@ -0,0 +1,4 @@
+inp = input("Enter the word: ")
+output = inp[::-1]
+print(f"Input word: {inp}")
+print(f"Output word: {output}")
